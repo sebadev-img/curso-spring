e7ef4a7ab62d2b88d2f051a8898d0f0c2d5289e0
sebadev-img <sebastiandev.img@gmail.com>
2022-07-21 00:34:05 -0300

    ejercicio 1 resuelto

---
+++ ejercicio1/src/main/java/com/sebadev/Main.java
@@ -0,0 +1,13 @@
+package com.sebadev;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Main {
+
+    public static void main(String[] args) {
+        ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
+        Saludo saludo = context.getBean("saludo", Saludo.class);
+        saludo.imprimirSaludo();
+    }
+}
---
+++ ejercicio1/src/main/java/com/sebadev/Saludo.java
@@ -0,0 +1,8 @@
+package com.sebadev;
+
+public class Saludo {
+
+    public void imprimirSaludo() {
+        System.out.println("Hola Mundo");
+    }
+}
